dfa2717385d1ec0e724e96a3f4abf27117897672
jontze <42588836+jontze@users.noreply.github.com>
2023-04-26 00:56:20 +0200

    feat(Notification): Connect gh notification api with codeblock

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
 import { Notice, Plugin } from "obsidian";
-import { AddPersonModal, SelectPersonModal } from "./modals";
+import { Observable, Subscription } from "rxjs";
 import {
 	DEFAULT_SETTINGS,
 	JontzePluginSettings,
 	JontzeSettingTab,
 } from "./Settings";
+import { GithubApi } from "./api";
+import { Notifications } from "./api/github/models";
+import { AddPersonModal, SelectPersonModal } from "./modals";
+import { getTokenPath } from "./token";
 
 export default class JontzePlugin extends Plugin {
+	private githubApi!: GithubApi;
+	private notifications$!: Observable<Notifications>;
+	private readonly sub = new Subscription();
+
 	settings!: JontzePluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.githubApi = new GithubApi(
+			getTokenPath(this.settings.secretDirectory)
+		);
+		this.notifications$ = this.githubApi.watchNotifications(
+			this.settings.githubNotificationRefreshRate,
+			true
+		);
 
 		const ribbonIconEl = this.addRibbonIcon(
 			"bot",
@@ -21,6 +36,43 @@ export default class JontzePlugin extends Plugin {
 			}
 		);
 
+		this.registerCommands();
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new JontzeSettingTab(this.app, this));
+
+		this.registerMarkdownCodeBlockProcessor(
+			"github-notifications",
+			(src, el, ctx) => {
+				this.sub.add(
+					this.notifications$.subscribe((notifications) => {
+						notifications.forEach((notify) => {
+							el.appendChild(createSpan({ text: notify.reason }));
+						});
+					})
+				);
+			}
+		);
+	}
+
+	onunload() {
+		console.log("Jontze: Unloading plugin");
+		this.sub.unsubscribe();
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	private registerCommands(): void {
 		this.addCommand({
 			id: "jontze-add-person",
 			name: "Add a new person",
@@ -42,24 +94,5 @@ export default class JontzePlugin extends Plugin {
 				).open();
 			},
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new JontzeSettingTab(this.app, this));
-	}
-
-	onunload() {
-		console.log("Jontze: Unloading plugin");
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 }
