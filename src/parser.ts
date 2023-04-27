2047be78c3f63458befaa705e6c97f4ebab7735d
jontze <42588836+jontze@users.noreply.github.com>
2023-04-28 00:47:09 +0200

    feat(parser): Add more elements to yaml query

--- src/parser.ts
+++ src/parser.ts
@@ -3,6 +3,17 @@ import { parse } from "yaml";
 export interface QueryObject {
 	filter?: {
 		unread?: boolean;
+		reason?: string[] | string;
+		repo?: {
+			name?: string[] | string;
+			owner?: string[] | string;
+			fullname?: string;
+			fork?: boolean;
+			private?: boolean;
+		};
+		subject?: {
+			type: string[] | string;
+		};
 	};
 }
 
